2266a5591c21bf13902dbc0b3beae7f301e6284b
Flebory <123499686+Flebory@users.noreply.github.com>
2026-06-23 13:29:36 -0400

    Added shadow underneath download buttons

--- client/src/components/ReportPreview.tsx
+++ client/src/components/ReportPreview.tsx
@@ -110,16 +110,16 @@ const ReportPreview: React.FC<ReportPreviewProps> = ({ facilityData, manualData 
 
     return (
         <div>
-            <div className="flex justify-center gap-2 shadow-md mb-2 px-4 py-4">
+            <div className="flex justify-center gap-2 mb-2 px-4 py-4">
                 <button 
                 onClick={handleDownloadPdf}
-                className="bg-green-600 text-white px-6 py-2 rounded-lg hover:bg-green-700 transition"
+                className="bg-green-600 text-white px-6 py-2 rounded-lg shadow-md hover:bg-green-700 transition"
                 >
                     Download PDF
                 </button>
                 <button
                     onClick={handleDownloadDocx}
-                    className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition"
+                    className="bg-blue-600 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-700 transition"
                 >
                     Download Word Doc
                 </button>
